617ee6da36c168f51c73f1268fee6c0caaeab8fa
Jason Spafford <nullprogrammer@gmail.com>
2013-06-04 14:59:20 -0700

    Updated install script to install recursively

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Installing to $HOME"
 
-echo "Installing root files to $HOME/"
-cp -r ./root-files/. $HOME/
+echo "Installing root-files content to $HOME/"
+cp -R ./root-files/. $HOME/
 
-echo "Installing .git files to $HOME/.git/"
-#cp -r ./git/ %HOME/.git/
+echo "Installing git content to $HOME/.git/"
+#cp -R ./git/. %HOME/.git/
 
-echo "Installing .vim folder's content to $HOME/.vim/"
-#cp -r ./vim/ %HOME/.vim/
+echo "Installing vim content to $HOME/.vim/"
+#cp -R ./vim/. %HOME/.vim/
