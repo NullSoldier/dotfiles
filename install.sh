4374fba4088c43218149e7e4765de8e25f6ffb9a
Jason Spafford <nullprogrammer@gmail.com>
2013-06-04 14:42:51 -0700

    Changed install script to be more clear

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "Installing to $HOME"
 
-echo "Installing root files in $HOME"
+echo "Installing root files to $HOME/"
 cp -r ./root-files/. $HOME/
 
-echo "Installing .git files in $HOME/.git"
+echo "Installing .git files to $HOME/.git/"
 #cp -r ./git/ %HOME/.git/
 
-echo "Installing .vim folder's content in $HOME/.vim"
+echo "Installing .vim folder's content to $HOME/.vim/"
 #cp -r ./vim/ %HOME/.vim/
