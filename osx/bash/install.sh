bddb603340432057236df84f8871acbaf23a519a
NullSoldier <nullprogrammer@gmail.com>
2023-01-10 23:47:14 -0500

    Fix linking profile

--- osx/bash/install.sh
+++ osx/bash/install.sh
@@ -4,9 +4,7 @@ ln -f "$HEREP/.bash_home" "$OUT/.bash_home"
 ln -f "$HEREP/.bash_aliases" "$OUT/.bash_aliases"
 ln -f "$HEREP/.bash_functions" "$OUT/.bash_functions"
 
-# Only use profile to link to other things
+# Only use profile to link home and use home to link our files
 touch ~/.bash_profile && grep -qxF 'source ~/.bash_home' ~/.bash_profile || echo 'source ~/.bash_home' >> ~/.bash_profile
-touch ~/.bash_profile && grep -qxF 'source ~/.bash_keys' ~/.bash_profile || echo 'source ~/.bash_keys' >> ~/.bash_profile
-touch ~/.bash_profile && grep -qxF 'source ~/.bash_aliases' ~/.bash_profile || echo 'source ~/.bash_aliases' >> ~/.bash_profile
 
 source ~/.bash_profile
